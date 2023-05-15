2cceb8e9aad203b7fba948d8a7714ff360d6071a
vbegunova <veronikabegunova42@gmail.com>
2023-05-15 09:05:05 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,70 +1,47 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const firstDelay = document.querySelector('input[name="delay"]');
-const delayStep = document.querySelector('input[name="step"]');
-const amountProm = document.querySelector('input[name="amount"]');
-const start = document.querySelector('button[type="submit"');
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
 
-start.addEventListener('click', onClick);
-let stepDelay;
-let counter;
-
-function onClick(evt) {
+function onSubmit(evt) {
   evt.preventDefault();
 
-  const delay = Number(firstDelay.value);
-  const step = Number(delayStep.value);
-  const amount = Number(amountProm.value);
-  stepDelay = 0;
-  counter = 0;
+  const { delay, step, amount } = evt.currentTarget.elements;
+  let firstDelay = Number(delay.value);
+  const delayStep = Number(step.value);
+  const amountPromises = Number(amount.value);
 
-  if (delay < 0 || step < 0 || amount <= 0) {
-    alert('Please, fill the fields with valid values');
-  } else {
-    if (amount) {
-      start.disabled = true;
-    }
+  if (firstDelay < 0 || delayStep < 0 || amountPromises <= 0) {
+    Notify.warning('Please, fill the fields with valid values');
+    return;
+  }
 
-    setTimeout(() => {
-      stepDelay += delay;
-      counter += 1;
-      createPromise(counter, stepDelay);
-
-      if (counter === amount) {
-        return;
-      }
-
-      const intervalId = setInterval(() => {
-        stepDelay += step;
-        counter += 1;
-
-        if (counter === amount) {
-          start.disabled = false;
-          clearInterval(intervalId);
-        }
-        createPromise(counter, stepDelay);
-      }, step);
-    }, delay);
+  for (let i = 1; i <= amountPromises; i++) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          timeout: 5000,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          timeout: 5000,
+        });
+      });
+    firstDelay += delayStep;
   }
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
-  })
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
-        timeout: 10000,
-      });
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-        timeout: 10000,
-      });
-    });
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
